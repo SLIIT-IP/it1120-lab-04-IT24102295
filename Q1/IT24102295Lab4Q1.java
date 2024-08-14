d8b515704d5aac022d53cbf9468cf62f5997ab5b
IT24102295 <it24102295@my.sliit.lk>
2024-08-14 10:24:20 +0530

    IT24102295Lab4Q1.

---
+++ Q1/IT24102295Lab4Q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IT24102295Lab4Q1{
+      public static void main(String[] args){
+      
+      Scanner input = new Scanner(System.in);
+      
+      int number;
+   
+      System.out.print(" Enter a number: ");
+      number = input.nextInt();
+
+      if (number > 0)
+      {
+         System.out.print("The number is Positive.");
+      }
+      else if (number < 0)
+      { 
+         System.out.print("The number is negetive. ");
+      }
+      else
+      {
+         System.out.print("The number is Zero. ");
+      }
+      }
+}
